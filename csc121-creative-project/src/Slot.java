0b147b1b3da84d6c773c8d9c66b1d659e6064eed
Noah Mills <noah.mills@vikings.berry.edu>
2023-09-18 21:14:58 -0400

    changes mole color
    yep

--- csc121-creative-project/src/Slot.java
+++ csc121-creative-project/src/Slot.java
@@ -2,20 +2,27 @@
 import processing.core.PApplet;
 
 public class Slot {
-	int x, y; //position
-	int size;
-	boolean active; //Determine if slot is in used/queued
-	
-	public Slot(int x, int y, int size, boolean active) {
-		super();
-		this.x = x;
-		this.y = y;
-		this.size = size;
-		this.active = active;
-	}
-	
-	public PApplet draw(PApplet c) {
-		c.circle(x, y, size);
+    int x, y; //position
+    int size;
+    boolean active; //Determine if slot is in used/queued
+    int color;
+    
+    public Slot(int x, int y, int size, boolean active, int color) {
+        super();
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.active = active;
+        this.color = color;
+    }
+    public PApplet draw(PApplet c) {
+        c.fill(color);
+        c.circle(x, y, size);
         return c;
+    }
+    
+    void fillSlot(){
+    	color = 255;
+    	Mole mole = new Mole (this, false, 100);
     }
 }
